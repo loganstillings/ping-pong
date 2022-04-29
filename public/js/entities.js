8e1d79eaa453732a56d9dd1cca4e8395c9cf60d6
Logan Stillings <lstillin@u.rochester.edu>
2022-04-29 15:52:08 -0700

    Invert game to be vertically aligned

--- public/js/entities.js
+++ public/js/entities.js
@@ -7,19 +7,27 @@ function Node(id, x, y, width, height, color) {
     this.color = color;
     return this;
 }
-function Player(id, x, y, paddleWidth, paddleHeight, appWidth, color) {
+function Player(id, x, y, paddleWidth, paddleHeight, appHeight, color) {
     var self = new Node(id, x, y, paddleWidth, paddleHeight, color);
     self.points = 0;
-    self.isPressingLeft = false;
-    self.isPressingRight = false;
     self.isPressingDown = false;
     self.isPressingUp = false;
 
     self.update = () => {
-        if (self.isPressingRight && self.x < appWidth - paddleWidth) {
-            self.x += PADDLE_SPEED;
-        } else if (self.isPressingLeft && self.x > 0) {
-            self.x -= PADDLE_SPEED;
+        if (self.isPressingDown && self.y < appHeight - paddleHeight) {
+            // move paddle up
+            if (self.y + PADDLE_SPEED > appHeight - paddleHeight) {
+                self.y = appHeight - paddleHeight;
+            } else {
+                self.y += PADDLE_SPEED;
+            }
+        } else if (self.isPressingUp && self.y > 0) {
+            // move paddle down
+            if (self.y - PADDLE_SPEED < 0) {
+                self.y = 0;
+            } else {
+                self.y -= PADDLE_SPEED;
+            }
         }
     };
 
@@ -34,7 +42,8 @@ function Ball(id, x, y, ballWidth, ballHeight, appWidth, appHeight, color) {
     var self = new Node(id, x, y, ballWidth, ballHeight, color);
 
     self.initialize = () => {
-        self.speedX = 1;
+        // TODO randomize speed directions
+        self.speedX = 3;
         self.speedY = 1;
         self.x = x;
         self.y = y;
@@ -47,23 +56,23 @@ function Ball(id, x, y, ballWidth, ballHeight, appWidth, appHeight, color) {
         self.y += self.speedY;
 
         if (self.isCollidingWithWall()) {
-            self.speedX *= -1;
+            self.speedY *= -1;
         }
 
         if (self.isCollidingWithPaddle(player1, player2)) {
-            self.speedY *= -1;
-            if (self.speedY > -15 && self.speedY < 15) {
-                // capping the vertical speed at 15 so it doesn't skip the paddle. TODO: make this dynamic as well
-                self.speedY += self.speedY < 0 ? -0.5 : 0.5;
+            self.speedX *= -1;
+            if (self.speedX > -15 && self.speedX < 15) {
+                // capping the horizontal speed at 15 so it doesn't skip the paddle. TODO: make this dynamic as well
+                self.speedX += self.speedX < 0 ? -0.5 : 0.5;
             }
-            self.speedX += self.speedX < 0 ? -0.5 : 0.5;
+            self.speedY += self.speedY < 0 ? -0.5 : 0.5;
         }
 
         if (self.isScored()) {
             self.speedX = 0;
             self.speedY = 0;
             paused = true;
-            if (self.y <= 0) {
+            if (self.x <= 0) {
                 player2.points++;
             } else {
                 player1.points++;
@@ -77,24 +86,24 @@ function Ball(id, x, y, ballWidth, ballHeight, appWidth, appHeight, color) {
     };
 
     self.isCollidingWithWall = () => {
-        return self.x <= 0 || self.x >= appWidth - ballWidth;
+        return self.y <= 0 || self.y >= appHeight - ballHeight;
     };
 
     self.isCollidingWithPaddle = (player1, player2) => {
         // What happens when the ball is moving so fast that it skips over the paddle?
         if (
-            self.y >= player2.y &&
-            self.y <= appHeight &&
             self.x >= player2.x &&
-            self.x <= player2.x + player2.width
+            self.x <= appWidth &&
+            self.y >= player2.y &&
+            self.y <= player2.y + player2.height
         ) {
             return true;
         }
         if (
-            self.y <= player1.y + player1.height &&
-            self.y >= 0 &&
-            self.x >= player1.x &&
-            self.x <= player1.x + player1.width
+            self.x <= player1.x + player1.width &&
+            self.x >= 0 &&
+            self.y >= player1.y &&
+            self.y <= player1.y + player1.height
         ) {
             return true;
         }
@@ -102,7 +111,7 @@ function Ball(id, x, y, ballWidth, ballHeight, appWidth, appHeight, color) {
     };
 
     self.isScored = () => {
-        return self.y <= 0 || self.y >= appHeight - ballHeight;
+        return self.x <= 0 || self.x >= appWidth - ballWidth;
     };
 
     return self;
